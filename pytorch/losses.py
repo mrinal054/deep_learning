98741cc4a4624322ec26e857850e42ef6cbde4c9
Mrinal Kanti Dhar <mrinal054@gmail.com>
2022-07-31 22:05:41 -0500

    Update losses.py

--- pytorch/losses.py
+++ pytorch/losses.py
@@ -27,5 +27,20 @@ class WCELoss(nn.Module):
         
         Output
         --------
-        Returns weighted cross-entropy loss        
+        Returns weighted cross-entropy loss    
+        
+        Expanation
+        --------------        
+        weights = [1, 100, 150, 1.]
+        
+        # Get dimensions to reshape the weights
+        # E.g. if weights = [1, 100, 150, 1.], then reshape dims = [1, 4, 1, 1, 1] 
+        # Here 4 is no. of channels/classes or len(weights). All are same.
+        # 1s are for batch, height, width, and depth
+        
+        num_classes = len(weights)
+        
+        reshape_dims = [1] + [num_classes] + [1,1,1]
+        
+        weights = torch.tensor(weights) # dtype=torch.float32
         
