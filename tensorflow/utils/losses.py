1c3c90c3ec4500c50aed511535e191d70267a6b7
Mrinal Kanti Dhar <mrinal054@gmail.com>
2023-01-07 12:48:27 -0700

    Update losses.py

--- tensorflow/utils/losses.py
+++ tensorflow/utils/losses.py
@@ -1,1 +1,29 @@
-#
+import tensorflow as tf
+import tensorflow.keras.backend as K
+
+def dice_coef(y_true, y_pred, const=K.epsilon()):
+    '''
+    Sørensen–Dice coefficient for 2-d samples.
+    
+    Input
+    ----------
+        y_true, y_pred: predicted outputs and targets.
+        const: a constant that smooths the loss gradient and reduces numerical instabilities.
+        
+    '''
+    
+    # flatten 2-d tensors
+    y_true_pos = tf.reshape(y_true, [-1])
+    y_pred_pos = tf.reshape(y_pred, [-1])
+    
+    # get true pos (TP), false neg (FN), false pos (FP).
+    true_pos  = tf.reduce_sum(y_true_pos * y_pred_pos)
+    false_neg = tf.reduce_sum(y_true_pos * (1-y_pred_pos))
+    false_pos = tf.reduce_sum((1-y_true_pos) * y_pred_pos)
+    
+    # 2TP/(2TP+FP+FN) == 2TP/()
+    coef_val = (2.0 * true_pos + const)/(2.0 * true_pos + false_pos + false_neg)
+    
+    return coef_val
+  
+  
