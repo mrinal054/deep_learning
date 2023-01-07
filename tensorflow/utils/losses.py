ded85732d102caa96cf58e9f745685437f8c58b7
Mrinal Kanti Dhar <mrinal054@gmail.com>
2023-01-07 12:52:10 -0700

    Update losses.py

--- tensorflow/utils/losses.py
+++ tensorflow/utils/losses.py
@@ -30,4 +30,22 @@ def sce_loss(y_true, y_pred):
     return sce_loss
 
   
- 
+#%%
+def weighted_sce_loss(y_true, y_pred, num_classes=4, loss_weights=[1, 150, 100, 1.0]):
+
+    y_true = tf.image.convert_image_dtype(y_true, tf.float32)
+    y_pred = tf.image.convert_image_dtype(y_pred, tf.float32)
+    
+    print('----------------------', y_true.get_shape())
+    print('----------------------', y_pred.get_shape())
+        
+    # Weighted cross entropy: approach adapts following code: https://stackoverflow.com/questions/44560549/unbalanced-data-and-weighted-cross-entropy
+    ce_loss = tf.keras.metrics.categorical_crossentropy(y_true, y_pred)
+    
+    #ce_loss = tf.nn.softmax_cross_entropy_with_logits(logits=y_pred, labels=y_true)
+
+    weighted_loss = tf.reshape(tf.constant(loss_weights), [1, 1, 1, 1, num_classes]) # Format to the right size
+    weighted_one_hot = tf.reduce_sum(weighted_loss*y_true, axis = -1)
+    ce_loss = ce_loss * weighted_one_hot
+    
+    return tf.reduce_mean(ce_loss) # Get loss
